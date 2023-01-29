20632a2472911eb6960ccf38eed93eea6b4a880e
Yannis Katsaros <yannis@katsaros.io>
2023-01-29 11:11:19 -0600

    Remove lexeme, don't think we'll need it

--- camlox/lib/token.ml
+++ camlox/lib/token.ml
@@ -1,6 +1,6 @@
 type t =
   { kind: TokenKind.t
-  ; lexeme: string  (* not quite sure why we need this, but keeping it consistent with the book *)
+  ; col: int
   ; line: int (* the line number the lexeme was encountered *)
   }
   [@@deriving show, eq]
